cacb9fab6f6c00f15f80c9d91e4b09c9a64cfe92
aditya3011007 <108451255+aditya3011007@users.noreply.github.com>
2022-07-24 23:27:33 +0530

    Add files via upload

---
+++ server/database/connection.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const connectDB = async () => {
+    try{
+        // mongodb connection string
+        const con = await mongoose.connect("mongodb+srv://ars30:Aditya123@cluster0.xw6df.mongodb.net/?retryWrites=true&w=majority", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        });
+
+        console.log(`MongoDB connected : ${con.connection.host}`);
+    }catch(err){
+        console.log(err);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB
